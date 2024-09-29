2df4050d9110bba1e4e8f17dc3f7dbc61bf8534c
Shawn Park <sptres@Shawns-MacBook-Pro-2.local>
2024-09-28 20:50:43 -0700

    shadcn and messages

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,5 +1,6 @@
 import { type Message as TMessage } from 'ai/react';
 import Message from './Message';
+import { MessageSquare } from 'lucide-react';
 
 interface MessagesProps {
   messages: TMessage[];
@@ -7,7 +8,7 @@ interface MessagesProps {
 const Messages = ({ messages }: MessagesProps) => {
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
-      {messages ? (
+      {messages.length ? (
         messages.map((message, i) => (
           <Message
             key={i}
@@ -16,7 +17,13 @@ const Messages = ({ messages }: MessagesProps) => {
           />
         ))
       ) : (
-        <div></div>
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
+          <MessageSquare className="size-8 text-blue-500" />
+          <h3 className="font-semibold text-xl text-white">You are all set!</h3>
+          <p className="text-zinc-500 text-sm">
+            Ask your first question to get started.
+          </p>
+        </div>
       )}
     </div>
   );
